0631a46aa907f54dee595db15f1368978063f10e
TED Vortex (Teodor Eugen Dutulescu) <teodord@cpwonlinesolutions.com>
2015-10-08 23:28:03 +0100

    First working firewall implementation

--- deployment/puppet/manifest/development.pp
+++ deployment/puppet/manifest/development.pp
@@ -6,9 +6,31 @@ class { 'yum::repo::repoforge': }
 class { 'yum::repo::epel': }
 class { 'yum::repo::remi': }
 
+# Firewall manifest
+resources { 'firewall': purge => true }
+Firewall {
+  before  => Class['::rhel::firewall::post'],
+  require => Class['::rhel::firewall::pre'],
+}
+include ::rhel::firewall
+firewall { '100 allow openssh':
+  chain   => 'INPUT',
+  state   => ['NEW'],
+  dport   => '22',
+  proto   => 'tcp',
+  action  => 'accept',
+}
+firewall { '100 allow httpd':
+  chain   => 'INPUT',
+  state   => ['NEW'],
+  dport   => '80',
+  proto   => 'tcp',
+  action  => 'accept',
+}
+
 # Nginx manifest
 include ::nginx
 
-# Php
+# Php manifest
 class { 'yum::repo::remi_php56': }
 include ::php
